4c5faebbb936223e7adde4e9475421a8babd4a8f
Paul-Maxime <github@paulmaxime.fr>
2019-01-26 16:57:19 +0100

    Add the apple, speech bubble and dead nestling

--- src/scenes/level-scene.js
+++ src/scenes/level-scene.js
@@ -9,16 +9,27 @@ class LevelScene extends Phaser.Scene {
     preload() {
         console.log('[Level] Preloading')
         this.load.image('nestling', 'assets/nestling.png')
+        this.load.image('nestling_dead', 'assets/nestling_dead.png')
+        this.load.image('speech_bubble', 'assets/speech_bubble.png')
+        this.load.image('apple', 'assets/apple.png')
     }
 
     create() {
         console.log('[Level] Creating')
         this.add.text(0, 0, 'This is the game scene.', { fill: '#fff' })
         this.nestlings = []
-        this.nestlings.push(new Nestling(this))
+        this.addNestling()
+        this.addNestling()
+        this.addNestling()
     }
 
     update(timestamp, elapsed) {
-        // TODO update entities with the elapsed time.
+        for (const nestling of this.nestlings) {
+            nestling.update(timestamp, elapsed)
+        }
+    }
+
+    addNestling() {
+        this.nestlings.push(new Nestling(this, this.nestlings.length))
     }
 }
