87d6779a7b4f56d7a9684e25cb75f1a6c65f36bf
Paul-Maxime <github@paulmaxime.fr>
2019-01-26 17:16:07 +0100

    Select a fruit randomly

--- src/entities/nestling.js
+++ src/entities/nestling.js
@@ -1,32 +1,33 @@
 // SuperNest - nestling entity
 'use strict'
 
-const HUNGER_MIN_TIME = 2000;
-const HUNGER_MAX_TIME = 5000;
+const HUNGER_MIN_TIME = 2000
+const HUNGER_MAX_TIME = 5000
+const STARVATION_TIME = 3000
 
-const STARVATION_TIME = 3000;
+const FRUITS = ['apple', 'banana']
 
 class Nestling {
     constructor(scene, id) {
-        this.id = id;
+        this.id = id
         this.scene = scene
         this.sprite = scene.add.sprite(150 + id * 75, 100, 'nestling')
         this.speechBubble = scene.add.sprite(this.sprite.x + 30, this.sprite.y - 50, 'speech_bubble')
         this.speechBubble.visible = false
         this.foodSprite = scene.add.sprite(this.speechBubble.x, this.speechBubble.y - 5, 'apple')
         this.foodSprite.visible = false
-        this.isDead = false;
-        this.fillStomach();
+        this.isDead = false
+        this.fillStomach()
     }
 
     update(timestamp, elapsed) {
         if (!this.isDead) {
-            this.hunger -= elapsed;
+            this.hunger -= elapsed
             if (this.hunger < 0) {
                 if (!this.isStarving) {
-                    this.askForFood();
+                    this.askForFood()
                 } else {
-                    this.dieOfStarvation();
+                    this.dieOfStarvation()
                 }
             }
         }
@@ -34,17 +35,18 @@ class Nestling {
 
     fillStomach() {
         console.log(`[Nestling ${this.id}] Filling stomach`)
-        this.isStarving = false;
-        this.requestedFood = null;
-        this.hunger = Phaser.Math.Between(HUNGER_MIN_TIME, HUNGER_MAX_TIME);
+        this.isStarving = false
+        this.requestedFood = null
+        this.hunger = Phaser.Math.Between(HUNGER_MIN_TIME, HUNGER_MAX_TIME)
     }
 
     askForFood() {
         console.log(`[Nestling ${this.id}] Asking for food`)
         this.isStarving = true;
-        this.hunger = STARVATION_TIME;
-        this.requestedFood = 'apple'
+        this.hunger = STARVATION_TIME
+        this.requestedFood = FRUITS[Phaser.Math.Between(0, FRUITS.length - 1)]
         this.speechBubble.visible = true
+        this.foodSprite.setTexture(this.requestedFood)
         this.foodSprite.visible = true
     }
 
